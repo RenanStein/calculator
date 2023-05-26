629b36efd95a0a2c020cd20ed2a4986a9bac5fc4
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2023-05-26 18:01:42 -0300

    .

--- scripts/algorithm-calculator.js
+++ scripts/algorithm-calculator.js
@@ -6,9 +6,8 @@ function add(value){
 function calcular(){
     let total = window.document.getElementById('box_number').innerHTML
 
-    let numero = Number(total.value)
-    if(numero){
-        window.document.getElementById('box_number').innerHTML = numero
+    if(total){
+        window.document.getElementById('box_number').innerHTML = Number(total)
     } else {
         window.document.getElementById('box_number').innerHTML = "[Erro]"
     }
